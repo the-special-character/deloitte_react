6465f1a8cf167079091b559ec035d85a6da4e666
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-09-12 10:35:21 +0530

    day1 -  b-1

---
+++ hoiting&scoping.js
@@ -0,0 +1,95 @@
+// HOISTING
+
+// globalThis.a = 1;
+
+// var a = 1;
+
+// var a = 2;
+
+// Converted Code
+
+// Variable Hoisting
+// var a;
+
+// a = 1
+
+// a = 2;
+
+// Global Scope
+// Function Scopt
+// block scope is not working in javascript
+
+// if(true) {
+//     var c = 10;
+
+// }
+
+// console.log(c);
+
+// var b;
+// b = 10;
+
+// function abc() {
+//     var b;
+//     console.log(b);
+//     b = 5;
+// }
+
+// abc()
+
+
+
+
+
+
+
+
+
+// var a = 1;
+
+// var a = 2;
+
+// console.log(a);
+
+// self calling function
+
+
+// (function() {
+//     var d = 1;
+// })()
+
+// console.log(a);
+
+// var a = 1;
+
+
+// let a = 1;
+
+// a = 2;
+
+// console.log(a);
+
+// const pi = 3.14
+
+// pi = 2.12
+
+// if(true) {
+//     let b = 5;
+// }
+
+// console.log(b);
+
+
+//              hoisting      block-scope   redeclare   reassign      
+
+// var             yes             no           yes        yes
+
+// let             no              yes          no          yes
+
+// const            no              yes         no          no
+
+
+
+
+
+
