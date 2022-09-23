fae2af2bf0fce39a38a05d2f3fa8903dbb4839af
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-09-23 13:21:48 +0530

    setup testing

--- src/component/ratting/index.tsx
+++ src/component/ratting/index.tsx
@@ -16,6 +16,7 @@ const Ratting = ({ rate, count, displayReviewCount }: Props) => {
         <div className="flex items-center">
           {[0, 1, 2, 3, 4].map((rating) => (
             <StarIcon
+              data-testid="rating-icon"
               key={rating}
               className={cn('h-5 w-5 flex-shrink-0', {
                 'text-orange-500': Math.round(rate) > rating,
@@ -28,6 +29,7 @@ const Ratting = ({ rate, count, displayReviewCount }: Props) => {
         <p className="sr-only">{rate} out of 5 stars</p>
         {displayReviewCount && (
           <a
+            data-testid="reviews-count"
             href="#"
             className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
           >
